872d09635cb1dc4f9f7f0ea6eaf9a312e898c2a2
Albert <42963174+albertZubov@users.noreply.github.com>
2019-11-08 14:35:23 +0300

    Корректировка насыщенности статистики, щлифовка кода

--- js/stat.js
+++ js/stat.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
@@ -7,11 +9,10 @@ var FONT_GAP = 15;
 var FONT_TEXT = '16px PT Mono';
 var BAR_WIGHT = 40;
 var BAR_DISTANSE = 50;
-var BAR_HEIGHT =-CLOUD_HEIGHT + (GAP * 6) + (FONT_GAP * 4);
-var TIME_Y = CLOUD_HEIGHT - GAP - FONT_GAP;
+var BAR_HEIGHT = CLOUD_HEIGHT - (GAP * 6) - (FONT_GAP * 4);
 
 
-var renderCloud = function(ctx, x, y, color) {
+var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
@@ -21,14 +22,18 @@ var renderText = function (ctx, x, y) {
   ctx.textBaseline = 'hanging';
   ctx.font = FONT_TEXT;
   ctx.fillText('Ура вы победили!', x, y);
-  ctx.fillText('Список результатов:', x, y + GAP*2);
+  ctx.fillText('Список результатов:', x, y + GAP * 2);
 };
 
-function getRandomArbitrary(min, max) {
+function getRandomValue(min, max) {
   return Math.random() * (max - min) + min;
 }
 
-var getMaxElement = function(arr) {
+function getPercentOfTime(maxTime, currentTime) {
+  return currentTime / maxTime * 100;
+}
+
+var getMaxElement = function (arr) {
   var maxElement = arr[0];
 
   for (var i = 0; i < arr.length; i++) {
@@ -40,29 +45,32 @@ var getMaxElement = function(arr) {
   return maxElement;
 };
 
-window.renderStatistics = function(ctx, names, times) {
+window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
   renderText(ctx, CLOUD_X + GAP * 2, CLOUD_Y + FONT_GAP);
 
   var maxTime = getMaxElement(times);
+  var color = getRandomValue(0, 255);
 
   ctx.fillStyle = '#000';
 
-  for (var i=0; i<names.length; i++) {
+  for (var i = 0; i < names.length; i++) {
+    var name = names[i];
+    var time = times[i];
+    var columnPositionX = CLOUD_X + BAR_DISTANSE - GAP + (BAR_WIGHT + BAR_DISTANSE) * i;
+    var columnPositionY = (-BAR_HEIGHT * time) / maxTime;
 
-    if (names[i] === 'Вы') {
+    if (name === 'Вы') {
       ctx.fillStyle = '#ff0000';
     } else {
-      ctx.fillStyle = 'hsla(240,' + (getRandomArbitrary(30, 100)) + '%, 50%, 1)';
+      var percent = getPercentOfTime(maxTime, time);
+      ctx.fillStyle = 'hsla(' + color + ', ' + percent + '%, 50%, 1)';
     }
-
-    var columnPositionX = CLOUD_X + BAR_DISTANSE - GAP + (BAR_WIGHT + BAR_DISTANSE)*i;
-    var columnPositionY = (BAR_HEIGHT*times[i])/maxTime;
 
     ctx.fillRect(columnPositionX, CLOUD_HEIGHT - FONT_GAP - GAP, BAR_WIGHT, columnPositionY);
     ctx.fillStyle = '#000';
-    ctx.fillText(names[i], columnPositionX, CLOUD_HEIGHT - FONT_GAP);
-    ctx.fillText(Math.round(times[i]), columnPositionX, (CLOUD_HEIGHT - BAR_DISTANSE + columnPositionY));
+    ctx.fillText(name, columnPositionX, CLOUD_HEIGHT - FONT_GAP);
+    ctx.fillText(Math.round(time), columnPositionX, CLOUD_HEIGHT - BAR_DISTANSE + columnPositionY);
   }
 };
